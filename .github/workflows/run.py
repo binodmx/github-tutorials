30788e126b154b51babcc2750b90d1bed1f557ca
Binod Karunanayake <36144069+binodmx@users.noreply.github.com>
2023-03-07 13:47:45 +0530

    Update run.py

--- .github/workflows/run.py
+++ .github/workflows/run.py
@@ -2,19 +2,22 @@ import os
 import requests
 
 # Get all existing labels in the repository
-labels = []
 all_existing_labels = []
-response = requests.get('https://api.github.com/repos/wso2/api-manager/labels', headers={"Accept": "application/vnd.github.v3+json"})
-if response.status_code == 200:
-    for label in response.json():
-        all_existing_labels.append(label["name"])
+for retry_count in range(3):
+    response = requests.get('https://api.github.com/repos/wso2/api-manager/labels', headers={"Accept": "application/vnd.github.v3+json"})
+    if response.status_code == 200:
+        for label in response.json():
+            all_existing_labels.append(label["name"])
+        break
+    else:
+        continue
 else:
     exit(0)
 
 # Get affected component
-i = os.environ["ISSUE_BODY"].index("### Affected Component")
+i = os.environ["ISSUE_BODY"].index("### Affected Component") + 23
 j = os.environ["ISSUE_BODY"].index("### Version")
-component = os.environ["ISSUE_BODY"][i+23:j].strip()
+component = os.environ["ISSUE_BODY"][i:j].strip()
 component_label = "Component/" + component
 if component_label in all_existing_labels:
     labels.append(component_label)
@@ -26,4 +29,7 @@ if affected_label in all_existing_labels:
     labels.append(affected_label)
 
 # Return verified labels
-print(",".join(labels))
+if len(labels) > 0:
+    print(",".join(labels))
+else:
+    print("Missing/Component")
