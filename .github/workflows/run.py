010eb8891ab394efd49a1b7ed60ca95e0ac65cb9
Binod Karunanayake <36144069+binodmx@users.noreply.github.com>
2023-03-23 15:04:09 +0530

    Update run.py

--- .github/workflows/run.py
+++ .github/workflows/run.py
@@ -3,6 +3,7 @@ import requests
 
 # Get all existing labels in the repository
 all_existing_labels = []
+debug = []
 for retry_count in range(3):
     response = requests.get('https://api.github.com/repos/wso2/api-manager/labels', headers={"Accept": "application/vnd.github.v3+json"})
     if response.status_code == 200:
@@ -19,21 +20,20 @@ i = os.environ["ISSUE_BODY"].index("### Affected Component") + 23
 j = os.environ["ISSUE_BODY"].index("### Version")
 component = os.environ["ISSUE_BODY"][i:j].strip()
 component_label = "Component/" + component
-# Debug
-print(component_label)
+debug.append(component_label)
 if component_label in all_existing_labels:
     labels.append(component_label)
 
 # Get component version
 version = os.environ["ISSUE_BODY"][j+13:j+18].strip()
 affected_label = "Affected/" + component + "-" + version
-# Debug
-print(affected_label)
+debug.append(affected_label)
 if affected_label in all_existing_labels:
     labels.append(affected_label)
 
+print(debug)
 # Return verified labels
-if len(labels) > 0:
-    print(",".join(labels))
-else:
-    print("Missing/Component")
+# if len(labels) > 0:
+#     print(",".join(labels))
+# else:
+#     print("Missing/Component")
